9616f9bc3a474a08002c95464d8d5590364ce266
Sankar-2006 <umasankarg2006@gmail.com>
2024-09-11 22:26:15 +0530

    add: Some functions

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const adviceBox = document.querySelector('#advice-text span');
+const idBox = document.querySelector('#top-text span')
+const diceButton = document.querySelector('#dice-button')
+
+adviceBox.innerHTML = "Loading advice...";
+idBox.innerHTML = "-";
+
 
 fetch(`https://api.adviceslip.com/advice`).then((res) => {
   return res.json();
 }).then(data => {
-  console.log(data['slip']['advice'])
   const advice = data['slip']['advice'];
+  const id = data['slip']['id']
   adviceBox.innerHTML = advice;
+  idBox.innerHTML = id;
+}).catch(error => {
+  adviceBox.innerHTML = "Failed to fetch advice. Please try again.";
 });
+
+
+diceButton.onclick = () => {
+  location.reload();
+}
