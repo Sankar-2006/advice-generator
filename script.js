4c1b44c8ef4942c79593216ec86be5e81fb77e39
Sankar-2006 <umasankarg2006@gmail.com>
2024-09-12 18:57:01 +0530

    edit: optimization

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const adviceBox = document.querySelector('#advice-text span');
-const idBox = document.querySelector('#top-text span')
-const diceButton = document.querySelector('#dice-button')
+const adviceBox = document.querySelector(".advice-text");
+const idBox = document.querySelector(".top-text span");
+const diceButton = document.querySelector(".dice-button");
 
-adviceBox.innerHTML = "Loading advice...";
-idBox.innerHTML = "-";
-
-
-fetch(`https://api.adviceslip.com/advice`).then((res) => {
-  return res.json();
-}).then(data => {
-  const advice = data['slip']['advice'];
-  const id = data['slip']['id']
-  adviceBox.innerHTML = advice;
-  idBox.innerHTML = id;
-}).catch(error => {
-  adviceBox.innerHTML = "Failed to fetch advice. Please try again.";
-});
-
-
-diceButton.onclick = () => {
-  location.reload();
+const fetchAdvice = () => {
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const advice = data["slip"]["advice"];
+      const id = data["slip"]["id"];
+      adviceBox.innerHTML = "\"" + advice + "\"";
+      idBox.innerHTML = id;
+    })
+    .catch((error) => {
+      adviceBox.innerHTML = "Failed to fetch advice. Please try again.";
+    });
 }
+
+fetchAdvice();
+
+diceButton.onclick = fetchAdvice;
